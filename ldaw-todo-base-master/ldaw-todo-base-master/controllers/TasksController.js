808ccece1304c2aead9571b094873e7ecc81292f
Juansgf <33111136+Juansgf@users.noreply.github.com>
2020-09-19 21:30:23 -0500

    Ok

---
+++ ldaw-todo-base-master/ldaw-todo-base-master/controllers/TasksController.js
@@ -0,0 +1,14 @@
+const Task = require('../models/Task');
+
+exports.store = (req, res) => {
+  let task = {};
+  task.description = req.body.description;
+  Task.create(task).then((id) => {
+    console.log('Task created with id: ', id);
+    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+      Task.find(id).then((task) => res.json(task));
+    } else {
+      res.redirect('/');
+    }
+  });
+}
